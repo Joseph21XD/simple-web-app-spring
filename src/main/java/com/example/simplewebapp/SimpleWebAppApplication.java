7055b3bdf271e4e02cba958aab59b55350f1aa4b
Joseph Ramirez <ramirezajoseph21@gmail.com>
2022-02-25 17:06:38 -0600

    Initial commit

---
+++ src/main/java/com/example/simplewebapp/SimpleWebAppApplication.java
@@ -0,0 +1,32 @@
+package com.example.simplewebapp;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class SimpleWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleWebAppApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+        return args -> {
+            System.out.println("Inspect Beans");
+            String[] beansNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beansNames);
+            for(String beanName : beansNames){
+                System.out.println(beanName);
+            }
+        };
+    }
+
+
+
+}
